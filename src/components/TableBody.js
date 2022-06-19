c9ea5da6e8aa42786d3113f7a784317c66d2b992
youniworst <oleg13kozak2207@gmail.com>
2022-06-20 02:33:28 +0300

    style: TableBody component brought to the general style

--- src/components/TableBody.js
+++ src/components/TableBody.js
@@ -2,23 +2,34 @@ import React from "react";
 import TableRow from "./TableRow";
 export default class TableBody extends React.Component {
     constructor(props) {
-        super(props)
+        super(props);
         this.state = {
-            modaleOpenedId: null
-        }
+            modaleOpenedId: null,
+        };
     }
 
     toggleModal = (id = null) => {
-        this.setState({ modaleOpenedId: id })
-    }
+        this.setState({ modaleOpenedId: id });
+    };
 
     render() {
         return (
             <tbody>
                 {this.props.list.map((element) => {
-                    return <TableRow toggleModal={this.toggleModal} openedId={this.state.modaleOpenedId} onSave={this.props.onSave} key={element.id} setCanChange={this.setCanChange} isTd={true} canChange={true} list={element} />
+                    return (
+                        <TableRow
+                            toggleModal={this.toggleModal}
+                            openedId={this.state.modaleOpenedId}
+                            onSave={this.props.onSave}
+                            key={element.id}
+                            setCanChange={this.setCanChange}
+                            isTd={true}
+                            canChange={true}
+                            list={element}
+                        />
+                    );
                 })}
             </tbody>
-        )
+        );
     }
 }
